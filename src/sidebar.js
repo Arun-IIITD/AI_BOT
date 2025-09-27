c716dd3c6f476c34fae4e22f735313e675a032b6
Arun Yadav <ay20552291@gmail.com>
2025-09-27 10:09:22 +0530

    sixth commit

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 
 const Sidebar = ({ chatSessions, loadChat, startNewChat }) => {
+  const navigate = useNavigate();
   return (
     <div className="sidebar">
        <button
